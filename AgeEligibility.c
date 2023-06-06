06b1e7bf5d6eef9bf3cf32054783d9e822a380a9
nilamshinde252005 <nilamshinde252005@gmail.com>
2023-06-06 17:26:54 +0530

    Added new class AgeEligibility
    To check age eligibility for vote

---
+++ AgeEligibility.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ void main()
+ {
+    int age;
+    printf("enter age");
+    scanf("%d",&age);
+    if(age>19)
+    printf("eligible to vote");
+    else
+    printf("not eligible to vote");
+ }

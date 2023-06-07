bea4969ab0e231d4bd44ba56675e183c4228227f
nilamshinde252005 <nilamshinde252005@gmail.com>
2023-06-07 14:23:33 +0530

    added SpCp

--- SpCp.c
+++ SpCp.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 void main()
 {
-    int sp,cp;
+    int sp,cp ,loss,profit;
     printf("enter cost price");
-    scanf()
+    scanf("%d%d",&sp,&cp);
+    if(sp>cp)
+    {
+        profit=sp-cp;
+        printf("there is a profit of %d",profit);
+    }
+        if(cp>sp)
+    {
+        loss=cp-sp;
+        printf("there is a loss of %d",loss);
+    }
+
 }

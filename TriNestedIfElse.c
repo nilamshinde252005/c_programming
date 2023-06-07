6b37401f0b50b9baeff6be45be3ac4272238b73f
nilamshinde252005 <nilamshinde252005@gmail.com>
2023-06-07 14:38:44 +0530

    added for TriNestedIfElse
    check tri is eq by nested if else

---
+++ TriNestedIfElse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter 3 angles");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a==60 )
+ {
+     if(b==60)
+    {
+        if(c==60)
+    printf("tri is eq");
+    
+    else 
+    printf("tri is not eq for c");
+ }
+  printf("    tri is not eq for b");
+}
+  printf("  tri is not eq for a");
+}

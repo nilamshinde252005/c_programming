b81b193cdf4cb6cc83131e146c80a96281500342
nilamshinde252005 <nilamshinde252005@gmail.com>
2023-06-09 22:00:02 +0530

    CountinueStatement

---
+++ CountinueStatement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=1;i<=10;i++)
+    {
+        if (i==5)
+    
+     continue;
+     else if (i==8)
+     break;
+     printf("%d\n",i);
+     }
+  printf("\n control is out of loop");
+}

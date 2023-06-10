d0abc0f5a2348ec2539ae3849cb6f0e1d029d978
nilamshinde252005 <nilamshinde252005@gmail.com>
2023-06-10 17:03:32 +0530

    swaping

---
+++ swaping.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x=100,y=200;
+    printf("before swaping x=%d y=%d,x,y");
+    x=x+y;
+    y=x-y;
+    x=x+y;
+    printf("\n\nafter swaping x=%d y=%d,x,y");
+}
